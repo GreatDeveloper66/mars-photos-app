29efc40f7e4b0185e324489ef5eabfab15c1466b
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-13 11:33:36 -0500

    Add mars api fetch statements

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 // server.js
 //const routes = require('./routes')
+require('dotenv').config()
+const [ URL, key ] = [ process.env.BASE_URL, process.env.MARS_API_KEY ]
 const path = require('path')
 const express = require('express');
 const app = express();
@@ -18,6 +20,18 @@ app.get('/', (req, res) => {
 	res.send('Hello from MERN');
 });
 */
+
+app.get('/sol/:sol/camera/:camera', (req,res) => {
+    const { sol, camera } = req.params
+    const fetchURL = `${URL}sol=${sol}&camera=${camera}&api_key=${key}`
+    console.log(fetchURL)
+    fetch(fetchURL)
+        .then(resp => resp.json())
+        .then(data => res.send(data))
+        .catch(error => console.log(error))
+    
+})
+
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
