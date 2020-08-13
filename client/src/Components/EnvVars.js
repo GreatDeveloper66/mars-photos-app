4e59ea00920e8c773cb02f1e1b0b2758cff2ae53
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-13 12:58:35 -0500

    Change url in EnvVars

--- client/src/Components/EnvVars.js
+++ client/src/Components/EnvVars.js
@@ -1,3 +1,3 @@
 import dotenv from 'dotenv'
 dotenv.config()
-export const URL = process.env.REACT_APP_APIURL || `http://localhost:${process.env.PORT}`
+export const URL = process.env.REACT_APP_APIURL || process.env.URL
