04b55c3c3fffc8b4c0e1c55d0cebe3e7ae77e8c2
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-12 20:45:01 -0500

    Add launch for front end

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 // server.js
 const routes = require('./routes')
+const path = require('path')
 const express = require('express');
 const app = express();
 
@@ -9,11 +10,19 @@ const PORT = process.env.PORT || 5000;
 // configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client","build")))
 
 // routes
+/*
 app.get('/', (req, res) => {
 	res.send('Hello from MERN');
 });
+*/
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
+
 
 // Bootstrap server
 app.listen(PORT, () => {
