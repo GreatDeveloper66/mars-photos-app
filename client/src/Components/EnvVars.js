2ff9a7dca54830c957ea7e103ce9948edfce0bef
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-13 12:27:35 -0500

    Add process.env.PORT referene in EnvVars.js

--- client/src/Components/EnvVars.js
+++ client/src/Components/EnvVars.js
@@ -1,3 +1,3 @@
 import dotenv from 'dotenv'
 dotenv.config()
-export const URL = process.env.REACT_APP_APIURL || 'http://localhost:5000'
+export const URL = process.env.REACT_APP_APIURL || `http://localhost:${process.env.PORT}`
