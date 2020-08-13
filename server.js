c547c72f3c08312ee4f21d614c90fc2d7c78c705
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-13 11:38:14 -0500

    Eliminate comments

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-
-// server.js
-//const routes = require('./routes')
 require('dotenv').config()
 const [ URL, key ] = [ process.env.BASE_URL, process.env.MARS_API_KEY ]
+const fetch = require('isomorphic-fetch')
 const path = require('path')
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client","build")))
-
-// routes
-/*
-app.get('/', (req, res) => {
-	res.send('Hello from MERN');
-});
-*/
 
 app.get('/sol/:sol/camera/:camera', (req,res) => {
     const { sol, camera } = req.params
@@ -32,13 +22,10 @@ app.get('/sol/:sol/camera/:camera', (req,res) => {
     
 })
 
-
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 
-
-// Bootstrap server
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}.`);
 });
