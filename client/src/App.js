aa3934ac060f3df00f8f3058e802497f1f0883d4
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-17 21:12:49 -0500

    Add component for Sign In screen

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Mars from './Components/Mars'
 import Register from './Components/Register'
-//import SignIn from './Components/SignIn'
+import SignIn from './Components/SignIn'
 //import 'bootstrap/dist/css/bootstrap.min.css'
 
 
