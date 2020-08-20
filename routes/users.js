c134c067a1473c86a51c5f8253db92be8e0154e2
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-20 16:38:02 -0500

    Add users routes

---
+++ routes/users.js
@@ -0,0 +1,34 @@
+const router = require('express').Router()
+const usersController = require('../controllers/usersController')
+
+
+router.route('/register').post(UsersController.create)
+
+
+
+
+
+
+
+
+
+
+/*
+
+const router = require('express').Router();
+const booksController = require('../controllers/booksController');
+
+router
+	.route('/')
+	.get(booksController.findAll)
+	.post(booksController.create);
+
+router
+	.route('/:id')
+	.get(booksController.findById)
+	.put(booksController.update)
+	.delete(booksController.remove);
+
+module.exports = router;
+
+*/
